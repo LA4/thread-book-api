5b4e6d6a3109dc63b43062db6a0e1113ec02f745
LA4 <andr.ludow.com>
2024-07-26 11:24:13 +0200

    other methods to workable cors

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express'
 import { join } from 'path';
+import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   app.useGlobalPipes(new ValidationPipe());
-  // CORS configuration
-  app.enableCors({
-    origin:'https://tread-book-front-web.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
+  app.use(cors({
+    origin:   
+ 'https://tread-book-front-web.vercel.app',
+    methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  }));
 
   // Start the application
   await app.listen(process.env.PORT || 3000);
