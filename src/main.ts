3675adf67a067f6c7660e62cd3f64846d9c5611d
LA4 <andr.ludow.com>
2024-07-30 16:01:38 +0200

    delete vercel file

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   app.useGlobalPipes(new ValidationPipe());
-  // app.enableCors({origin :"https://tread-book-front-web.vercel.app"})
-  app.use(cors({
-    origin:   
- 'https://tread-book-front-web.vercel.app',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+  app.enableCors({origin :"https://tread-book-front-web.vercel.app"})
+//   app.use(cors({
+//     origin:   
+//  'https://tread-book-front-web.vercel.app',
+//     methods: 'GET,POST,PUT,DELETE',
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//   }));
 
   // Start the application
   await app.listen(process.env.PORT || 3000);
